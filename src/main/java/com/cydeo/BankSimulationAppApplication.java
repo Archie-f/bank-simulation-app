e96b6755d85d3d3a50e75751acf46f924f5b7e78
Archie-f <akif.mk66@gmail.com>
2023-09-30 17:10:45 +0300

    account controller created

--- src/main/java/com/cydeo/BankSimulationAppApplication.java
+++ src/main/java/com/cydeo/BankSimulationAppApplication.java
@@ -18,26 +18,26 @@ public class BankSimulationAppApplication {
     public static void main(String[] args) {
         ApplicationContext container = SpringApplication.run(BankSimulationAppApplication.class, args);
 
-        //get the account and transaction service beans
-        AccountService accountService = container.getBean(AccountService.class);
-        TransactionService transactionService = container.getBean(TransactionService.class);
-
-        //create 2 accounts sender and receiver
-        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.SAVING,1L);
-        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.SAVING,2L);
-        Account receiver2 = null;
-
-        //see all created accounts first
-        accountService.lisAllAccounts().forEach(System.out::println);
-
-        //test if the conditions work and exceptions are thrown correctly
-        transactionService.makeTransfer(sender,receiver,BigDecimal.valueOf(40),new Date(),"Transaction 1");
-
-        //see transaction
-        System.out.println(transactionService.findAllTransaction().get(0));
-
-        //see all created accounts after transaction
-        accountService.lisAllAccounts().forEach(System.out::println);
+//        //get the account and transaction service beans
+//        AccountService accountService = container.getBean(AccountService.class);
+//        TransactionService transactionService = container.getBean(TransactionService.class);
+//
+//        //create 2 accounts sender and receiver
+//        Account sender = accountService.createNewAccount(BigDecimal.valueOf(70),new Date(), AccountType.SAVING,1L);
+//        Account receiver = accountService.createNewAccount(BigDecimal.valueOf(50),new Date(), AccountType.SAVING,2L);
+//        Account receiver2 = null;
+//
+//        //see all created accounts first
+//        accountService.lisAllAccounts().forEach(System.out::println);
+//
+//        //test if the conditions work and exceptions are thrown correctly
+//        transactionService.makeTransfer(sender,receiver,BigDecimal.valueOf(40),new Date(),"Transaction 1");
+//
+//        //see transaction
+//        System.out.println(transactionService.findAllTransaction().get(0));
+//
+//        //see all created accounts after transaction
+//        accountService.lisAllAccounts().forEach(System.out::println);
 
 
     }
---
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -0,0 +1,27 @@
+package com.cydeo.controller;
+
+import com.cydeo.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    /*
+        write a method to return index.html including account list information
+        endpoint:index
+     */
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @GetMapping("/index")
+    public String getIndexPage(Model model){
+
+        model.addAttribute("accountList", accountService.lisAllAccounts());
+        return "account/index";
+    }
+}
