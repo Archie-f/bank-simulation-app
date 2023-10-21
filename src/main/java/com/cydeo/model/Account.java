6c2d4416737415357c94ad276982d7356ad3db82
Archie-f <akif.mk66@gmail.com>
2023-10-21 19:39:58 +0300

    Validation started

--- src/main/java/com/cydeo/model/Account.java
+++ src/main/java/com/cydeo/model/Account.java
@@ -4,7 +4,9 @@ import com.cydeo.enums.AccountStatus;
 import com.cydeo.enums.AccountType;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.UUID;
@@ -14,9 +16,13 @@ import java.util.UUID;
 public class Account {
 
     private UUID id;
+    @NonNull
+    @Positive
     private BigDecimal balance;
+    @NonNull
     private AccountType accountType;
     private Date creationDate;
+    @NonNull
     private Long userId;
     private AccountStatus accountStatus;
 
