93a8834e182a4e058a5dc688d4210cbd31e570ea
Archie-f <akif.mk66@gmail.com>
2023-12-02 20:52:20 +0300

    orm: Repositories updated.

--- src/main/java/com/cydeo/repository/TransactionRepository.java
+++ src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -1,6 +1,7 @@
 package com.cydeo.repository;
 
 import com.cydeo.dto.TransactionDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -9,31 +10,31 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class TransactionRepository {
+public interface TransactionRepository extends JpaRepository {
 
-    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
-
-    public TransactionDTO save(TransactionDTO transactionDTO){
-        transactionDTOList.add(transactionDTO);
-        return transactionDTO;
-    }
-
-    public List<TransactionDTO> findAll() {
-        return transactionDTOList;
-    }
-
-    public List<TransactionDTO> findLast10Transactions() {
-        //write a stream that sort the transactions based on creation date
-        //and only return 10 of them
-        return transactionDTOList.stream()
-                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
-                .limit(10)
-                .collect(Collectors.toList());
-    }
-
-    public List<TransactionDTO> findTransactionListById(Long id) {
-        return transactionDTOList.stream()
-                .filter(transactionDTO -> transactionDTO.getSender().equals(id) || transactionDTO.getReceiver().equals(id))
-                .collect(Collectors.toList());
-    }
+//    public static List<TransactionDTO> transactionDTOList = new ArrayList<>();
+//
+//    public TransactionDTO save(TransactionDTO transactionDTO){
+//        transactionDTOList.add(transactionDTO);
+//        return transactionDTO;
+//    }
+//
+//    public List<TransactionDTO> findAll() {
+//        return transactionDTOList;
+//    }
+//
+//    public List<TransactionDTO> findLast10Transactions() {
+//        //write a stream that sort the transactions based on creation date
+//        //and only return 10 of them
+//        return transactionDTOList.stream()
+//                .sorted(Comparator.comparing(TransactionDTO::getCreateDate).reversed())
+//                .limit(10)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<TransactionDTO> findTransactionListById(Long id) {
+//        return transactionDTOList.stream()
+//                .filter(transactionDTO -> transactionDTO.getSender().equals(id) || transactionDTO.getReceiver().equals(id))
+//                .collect(Collectors.toList());
+//    }
 }
